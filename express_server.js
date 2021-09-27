3a67027674c74c79373ce38c97c845d80227b8a4
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-27 14:36:34 -0400

    add body-parser dependency

--- express_server.js
+++ express_server.js
@@ -17,14 +17,6 @@ app.listen(PORT, () => {
   console.log(`Example app listening ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -34,3 +26,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
