b663dc6df3ffa7e22d2f1d93a3985b7d5df5d1e7
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-27 14:43:13 -0400

    random string generator for shor url

--- express_server.js
+++ express_server.js
@@ -34,7 +34,8 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  console.log(req.body); 
+  const randomString = generateRandomString(); // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
